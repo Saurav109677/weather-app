a56c97bd96bbab19a9539ebf46e6b556e08399eb
Saurav109677 <34793927+Saurav109677@users.noreply.github.com>
2020-08-29 18:05:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,14 +9,14 @@ const forecast=require('./utils/forecast')
 const app=express()
 const port=process.env.PORT || 9000
 // Define paths for Express config
-const publicDirPath=path.join(__dirname,'../public')
-const viewsPath=path.join(__dirname,'../templates/views')
-const partialsPath=path.join(__dirname,'../templates/partials')
+// const publicDirPath=path.join(__dirname,'../public')
+// const viewsPath=path.join(__dirname,'../templates/views')
+// const partialsPath=path.join(__dirname,'../templates/partials')
 app.use(express.static('frontend'))
 
 // Setup handlebars engine and views location
 
-hbs.registerPartials(partialsPath)   //partial meaans the common tags .. like header or footer
+// hbs.registerPartials(partialsPath)   //partial meaans the common tags .. like header or footer
 
 //Setup static directory to serve
 // app.use(express.static(publicDirPath))      //for static pages
@@ -26,35 +26,35 @@ hbs.registerPartials(partialsPath)   //partial meaans the common tags .. like he
 
 app.use(cors())
 
-app.get('',(req,res) => {
-    //res.send('<h1>Hello Express!</h1>')\
-    res.render('index',{
-        name:'krsna',
-        title:'Weather App'
-    })
-})
+// app.get('',(req,res) => {
+//     //res.send('<h1>Hello Express!</h1>')\
+//     res.render('index',{
+//         name:'krsna',
+//         title:'Weather App'
+//     })
+// })
 
-app.get('/help',(req,res)=>{
-    // res.send([
-    //     {
-    //     name:'krsna',
-    //     age:22
-    // },
-    // {
-    //     name:'radhe',
-    //     age:22
-    // }
-    // ])
+// app.get('/help',(req,res)=>{
+//     // res.send([
+//     //     {
+//     //     name:'krsna',
+//     //     age:22
+//     // },
+//     // {
+//     //     name:'radhe',
+//     //     age:22
+//     // }
+//     // ])
 
-    res.render('help',{
-        message:'Hare Krsna !!',
-        title:'Help page'
-    })
-})
+//     res.render('help',{
+//         message:'Hare Krsna !!',
+//         title:'Help page'
+//     })
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('<h2>About page</h2>')
-})
+// app.get('/about',(req,res)=>{
+//     res.send('<h2>About page</h2>')
+// })
 
 app.get('/weather',(req,res)=>{
     if(!req.query.address){                 //https:localhost:9000/weather?address=new york
@@ -80,9 +80,9 @@ app.get('/weather',(req,res)=>{
    
 })
 
-app.get('*',(req,res)=>{
-    res.send('<h3>404 ERROR page!!</h3>')
-})
+// app.get('*',(req,res)=>{
+//     res.send('<h3>404 ERROR page!!</h3>')
+// })
 
 // app.com
 // app.com/help
